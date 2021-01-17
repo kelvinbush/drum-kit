2528a0fafe990d2ae17dbb948fce7168c9beb688
Kelvin Bush <kelybush@gmail.com>
2021-01-17 07:18:39 +0300

    key board event listeners for clicks complete

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 let noOfDrumButtons = document.querySelectorAll(".drum").length
 for (let i = 0; i < noOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        let buttonInnerHtml = this.innerHTML;
-
-        switch (buttonInnerHtml) {
-            case 'w':
-                new Audio("sounds/tom-1.mp3").play();
-                break;
-            case 'a':
-                new Audio("sounds/tom-2.mp3").play();
-                break;
-            case 's':
-                new Audio("sounds/tom-3.mp3").play();
-                break;
-            case 'd':
-                new Audio("sounds/tom-4.mp3").play();
-                break;
-            case 'j':
-                new Audio("sounds/snare.mp3").play();
-                break;
-            case 'k':
-                new Audio("sounds/crash.mp3").play();
-                break;
-            case 'l':
-                new Audio("sounds/kick-bass.mp3").play();
-                break;
-        }
+        getPressedKey(this.innerHTML);
     })
 }
 
+document.addEventListener("keydown", function (event) {
+    getPressedKey(event.key);
+})
+
+function getPressedKey(key) {
+    switch (key) {
+        case 'w':
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case 'a':
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case 's':
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case 'd':
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case 'j':
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case 'k':
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case 'l':
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
